eb99a41eba2e51742ef3fe39cbaa36f07e0f778a
erbpsy <erbpsy@gmail.com>
2021-12-12 10:14:34 +0530

    added functionality to read query string and write its values to the screen

--- urlmodule.js
+++ urlmodule.js
@@ -3,6 +3,14 @@ const url = require('url');
 
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(req.url);
+
+    //get query string data
+    var queryString = url.parse(req.url, true).query;
+
+    // get year and month values from query string
+    var queryStringData = "Year: " + queryString.year + " Month: " + queryString.month;
+
+    // write year and month data to the screen
+    res.write(queryStringData);
     res.end();
 }).listen(8080);
